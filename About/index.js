cbfdd9a2e8fe67c91d07916625f95eae4b0b771e
really_ant <115508192+jj123llol@users.noreply.github.com>
2026-06-19 18:22:38 -0500

    Update index.js

--- About/index.js
+++ About/index.js
@@ -14,3 +14,22 @@ document.querySelector("body > input").addEventListener('change', e => {
     }`
   }
 });
+const text = "Ｐｌａｓｍｉｉ＜３";
+let index = 0;
+let currentTitle = "";
+function animateTitle() {
+  if (index < text.length) {
+    currentTitle += text[index];
+    document.title = currentTitle;
+    index++;
+    setTimeout(animateTitle, 200);
+  } else {
+    setTimeout(() => {
+      index = 0;
+      currentTitle = "";
+      document.title = "";
+      animateTitle();
+    }, 3000);
+  }
+}
+animateTitle();
